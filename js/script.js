f2454db95b747f05d15bb2cc7410145c07fb1199
Khairaan07 <muhkhairaankhalishsuherman@gmail.com>
2025-01-17 01:09:14 +0800

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-// Greet the user
-document.addEventListener("DOMContentLoaded", () => {
- const greeting = document.querySelector(".banner p");
- greeting.textContent = `Welcome to my portfolio, MUH. KHAIRAAN KHAALISH S.!`;
-});
+const navSlide = () => {
+ const burger = document.querySelector(".burger");
+ const navLists = document.querySelector("nav");
 
-// Form validation
-document.getElementById("contactForm").addEventListener("submit", function (e) {
- e.preventDefault();
- const name = document.getElementById("name").value;
- const email = document.getElementById("email").value;
- const message = document.getElementById("message").value;
+ burger.addEventListener("click", () => {
+   // Toggle nav list and burger class
+   navLists.classList.toggle("nav-active");
+   burger.classList.toggle("toggle-burger");
+ });
+};
 
- if (name && email && message) {
-   alert(`Thank you, ${name}! Your message has been sent.`);
- } else {
-   alert("Please fill in all fields.");
+navSlide();
+
+window.onbeforeunload = () => {
+ for (const form of document.getElementsByTagName("form")) {
+   form.reset();
  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+ const welcomeMessage = document.getElementById("welcome-message");
+ const userName = "Khairaan"; // Nama dinamis dapat diganti
+ welcomeMessage.textContent = `Hai ${userName}, Selamat Datang`;
 });
